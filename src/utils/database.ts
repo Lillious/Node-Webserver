2440140da994a6a6a4d70a4e251f2617d8a18fab
Logan Brown <logan.brown@tylertech.com>
2023-03-05 18:57:26 -0500

    Added database functionality with support for ssl

---
+++ src/utils/database.ts
@@ -0,0 +1,30 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+const fs = require('fs');
+
+const pool = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    ssl: {
+        ca: fs.readFileSync(__dirname + '/ca-certificate.crt')
+    }
+});
+
+export const query = (sql: string, values: any) => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err: any, connection: any) => {
+            if (err) {
+                return reject(err);
+            }
+            connection.query(mysql.format(sql, values), (err: any, rows: any) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(rows);
+                }
+                connection.release();
+            });
+        });
+    });
+};
