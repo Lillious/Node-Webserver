a247e43935ce6ef8b8ffd57543437fd236bd4fe3
Logan Brown <logan.brown@tylertech.com>
2023-03-31 01:22:32 -0400

    Updated cookie domain to *.* to be dynamic

--- src/app.ts
+++ src/app.ts
@@ -52,7 +52,7 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
         path: '/',
-        domain: '.lillious.com' // Update this to your domain
+        domain: '*.*'
     },
     resave: true,
     saveUninitialized: true
