b495acafb0848eb35f607781eb59c307747186b6
Logan Brown <logan.brown@tylertech.com>
2023-05-25 16:05:08 -0400

    Fixed a bug with jobs

--- src/jobs/jobs.ts
+++ src/jobs/jobs.ts
@@ -5,6 +5,9 @@ import query from '../utils/database.js';
 import fs from 'fs';
 import tar from 'tar';
 import path from 'path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const job = {
     // Clear inactive sessions from the database
