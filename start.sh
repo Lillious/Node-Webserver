3d434d54c5d03ece1a5ca7ad1c91ea6d975e40fa
Logan Brown <logan.brown@tylertech.com>
2023-07-21 16:18:24 -0400

    Updated variables

--- start.sh
+++ start.sh
@@ -2,16 +2,15 @@ clear
 echo "Deployment Started"
 pm2 stop app 2>&1 >/dev/null
 echo "Removing Old Files"
-pm2 delete app 2>&1 >/dev/null
+pm2 delete Webserver 2>&1 >/dev/null
 echo "Building Application"
 npm run build 2>&1 >/dev/null
 echo "Starting Application"
-pm2 start ./app.sh 2>&1 >/dev/null
+pm2 start ./app.sh --name "Webserver" 2>&1 >/dev/null
 echo "Creating Daemon"
 pm2 app 2>&1 >/dev/null
 echo "Saving Configurations"
 pm2 save 2>&1 >/dev/null
 echo "Deployment Completed Successfully!"
-pm2 status app
 pm2 flush app 2>&1 >/dev/null
-pm2 logs app
+exit 0
