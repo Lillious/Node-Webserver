4742ca4a5cbf07a3109ab45b5d711f98afc827ce
Logan Brown <logan.brown@tylertech.com>
2023-07-26 02:40:47 -0400

    Added logging to file located in dist/logs

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,8 +1,50 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const info = (message: string) => {
+    const log = `${timestamp()} \x1b[32m${message}\x1b[0m`;
+    console.log(log);
+    WriteLogFile(log)
+};
+
+export const error = (message: string) => {
+    const log = `${timestamp()} \x1b[31m${message}\x1b[0m`;
+    console.error(log);
+    WriteLogFile(log)
+};
+
+export const warn = (message: string) => {
+    const log = `${timestamp()} \x1b[33m${message}\x1b[0m`;
+    console.warn(log);
+    WriteLogFile(log)
+};
+
+export const debug = (message: string) => {
+    const log = `${timestamp()} \x1b[34m${message}\x1b[0m`;
+    console.debug(log);
+    WriteLogFile(log)
+};
+
+const logDir = path.join(__dirname, '../../logs');
+if (!fs.existsSync(logDir)) {
+    try {
+        fs.mkdirSync(logDir);
+    } catch (err) {
+        error('Failed to create log directory');
+    }
+}
+
 const timestamp = () => {
     return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
 };
 
-export const info = (message: string) => console.log(`${timestamp()} \x1b[32m${message}\x1b[0m`);
-export const error = (message: string) => console.error(`${timestamp()} \x1b[31m${message}\x1b[0m`);
-export const warn = (message: string) => console.warn(`${timestamp()} \x1b[33m${message}\x1b[0m`);
-export const debug = (message: string) => console.debug(`${timestamp()} \x1b[34m${message}\x1b[0m`);
+function WriteLogFile (message: string) {
+    try {
+        fs.appendFileSync(path.join(__dirname, '../../logs/debug.log'), `${timestamp()} ${message}\n`);
+    } catch (err) {
+        error('Failed to write to debug.log');
+    }
+}
